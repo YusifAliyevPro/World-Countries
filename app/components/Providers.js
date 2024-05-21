23781f299f8c5b8fc0c24cf89ba904539886b9a7
Yusif Aliyev <yusifaliyevpro@gmail.com>
2024-05-21 23:28:11 +0400

    new udpates

--- app/components/Providers.js
+++ app/components/Providers.js
@@ -2,10 +2,16 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-
+import { MotionConfig } from "framer-motion";
 import { useRouter } from "next/navigation";
+import { isMobile } from "react-device-detect";
 
 export function Providers({ children }) {
   const router = useRouter();
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  const reducedMotion = isMobile ? "always" : "never";
+  return (
+    <NextUIProvider navigate={router.push}>
+      <MotionConfig reducedMotion={reducedMotion}>{children}</MotionConfig>
+    </NextUIProvider>
+  );
 }
